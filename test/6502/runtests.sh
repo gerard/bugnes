4f49a070e83cfa67a172003aefe911a9635624af
Gerard Lledo <gerard.lledo@gmail.com>
2009-11-15 19:41:31 +0200

    TEST: Remove old, superseded testing framework
    Do what was hinted in the previos commit.

--- test/6502/runtests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Filenames should not contain any dot other than the one in the "extension"
-
-set -e
-for BINARY in t/*.bin
-do
-    EXP_TRACE=`cut -d. -f1 <<< $BINARY`.trace
-    ./$1 $BINARY $EXP_TRACE > /dev/null
-done
