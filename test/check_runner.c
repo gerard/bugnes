e144459f75a5d77faa812816691b759c790465cc
Gerard Lledo <gerard.lledo@gmail.com>
2009-12-04 21:50:22 +0200

    TEST: Add test logging into a file and reduce build log output

--- test/check_runner.c
+++ test/check_runner.c
@@ -16,6 +16,7 @@ int main(void)
     srunner_add_suite(sr, simplenes);
     srunner_add_suite(sr, smoke);
 
+    srunner_set_log(sr, "check.log");
     srunner_run_all(sr, CK_MINIMAL);
 
     n_failed = srunner_ntests_failed(sr);
